fdd186715bd0750c8efcab43a0c13f2f4299f7d7
SuperRandomName <48484291+SuperRandomName@users.noreply.github.com>
2019-03-13 16:26:47 +0100

    Update StoreStart.java

--- StoreStart.java
+++ StoreStart.java
@@ -6,42 +6,38 @@ import lab5.simulator.events.Start;
 import lab5.store.StoreState;
 
 /**
- * @author Mikeal Granström, Erik Olausson, Amir Rakshan, Sermed Mutter
+ * @author Mikael Granström, Erik Olausson, Sermed Mutter, Amir Rakshan
  */
 
-/** 
- *  Denna skapar ett start event för butiken
+/**
+ * Detta är en klass för affärens starthändelse (event).
+ *
  */
-
 public class StoreStart extends Start {
 	
 	private StoreState state;
 	
-	
 	/**
-	 * Konstruktorn kallar på start konstruktor med tid, storeState och eventQueue
-	 * den lagrar en Customer, sätter namnet på eventet till Shopping och lägger till dess state.
-	 * @param time  - tiden för eventet 
-	 * @param storeState - simulatorn tillstånd
-	 * @param Customer - kunden som tillhör  händelsen.
-	 * @param eventQueue - den nuvarande händelsekön
+	 * Konstruktorn har parametrarna time, storeState och eventQueue.
+	 * Konstruktorn lagrar simuleringens tillstånd.
 	 * 
-	 */ 
+	 * @param time - Tiden för eventet.
+	 * @param storeState - Simulatorns tillstånd.
+	 * @param eventQueue - Den nuvarande händelsekön.
+	 */
 	public StoreStart(double time, StoreState storeState, EventQueue eventQueue) {
 		super(time, storeState, eventQueue);
 		this.state = storeState;
 	}
 	
-	
 	/**
-	 * Run metoden sätter en start tid och skapar en första arrival event. 
+	 * Metoden run sätter en starttid (vanligtvis 0) och lägger till det första Arrival-eventet.
 	 */
 	public void run() {
-		state.setTimeCurrent(TIME);		// sätter tiden till 0
+		state.setTimeCurrent(TIME);
 
 		state.setOpenCurrent(true);
-		// Creates new arrival event and puts it in the queue.
-		eventQueue.addEvent(new Arrival(state.getRandomCheckoutTime, state, eventQueue));
+		eventQueue.addEvent(new Arrival(state.getNewArrivalTime(), state, eventQueue));
 	}
 
 }
